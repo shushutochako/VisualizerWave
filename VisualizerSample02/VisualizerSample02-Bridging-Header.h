149e67be5b9947302daa1ce982e5b2626f87943a
増島 亘康 <masujima_nobuyasu_gn@cyberagent.co.jp>
2015-06-09 15:41:23 +0900

    Change directory layer

--- VisualizerSample02/VisualizerSample02-Bridging-Header.h
+++ VisualizerSample02/VisualizerSample02-Bridging-Header.h
@@ -1,7 +1,10 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  VisualizerSample02-Bridging-Header.h
+//  VisualizerSample02
 //
-
+//  Created by 増島 亘康 on 2015/06/09.
+//  Copyright (c) 2015年 増島 亘康. All rights reserved.
+//
 
 #import "SCSiriWaveformView.h"
 #import "SCViewController.h"
