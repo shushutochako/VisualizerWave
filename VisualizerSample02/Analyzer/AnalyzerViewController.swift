0abdce7defc6ab4f796eeb5cee966281174b1d04
増島 亘康 <masujima_nobuyasu_gn@cyberagent.co.jp>
2015-06-12 16:31:11 +0900

    Change

--- VisualizerSample02/Analyzer/AnalyzerViewController.swift
+++ VisualizerSample02/Analyzer/AnalyzerViewController.swift
@@ -10,25 +10,44 @@ import UIKit
 
 class AnalyzerViewController: ViewController {
   @IBOutlet weak var audioPlot: EZAudioPlot!
+  @IBOutlet weak var audioPlot2: EZAudioPlot!
   
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    //self.audioPlot.color = UIColor(red: 149, green: 255, blue: 239, alpha: 1)
-    self.audioPlot.color = UIColor.whiteColor()
-    //self.audioPlot.backgroundColor = UIColor.clearColor()
+    self.audioPlot.color = UIColor(red: 149/255.0, green: 255/255.0, blue: 239/255.0, alpha: 1)
     self.audioPlot.plotType  = .Rolling
     self.audioPlot.shouldFill = true
     self.audioPlot.shouldMirror = true
+    self.audioPlot.gain = 20.0
+
+    self.audioPlot2.color = UIColor(red: 149/255.0, green: 255/255.0, blue: 239/255.0, alpha: 1)
+    self.audioPlot2.plotType  = .Rolling
+    self.audioPlot2.shouldFill = true
+    self.audioPlot2.shouldMirror = true
 
     MicrophoneInputConverter.sharedInstance.delegate = self
     MicrophoneInputConverter.sharedInstance.startFetchingAudio()
   }
+  
+  func dispatch_async_main(block: () -> ()) {
+    dispatch_async(dispatch_get_main_queue(), block)
+  }
+  
+  func doubleByte(buffer: UnsafeMutablePointer<Float>, length: UInt32) {
+    for i in 0...Int(length)  {
+      buffer[i] = buffer[i] * 20
+    }
+  }
 }
+
 
 extension AnalyzerViewController : MicrophoneInputConverterDelegate {
   func onInputMicrophoneDate(buffer: UnsafeMutablePointer<UnsafeMutablePointer<Float>>, bufferSize: UInt32) {
-    self.audioPlot.updateBuffer(buffer[0], withBufferSize: bufferSize)
+    //self.doubleByte(buffer[0], length: bufferSize)
+    dispatch_async_main {
+      self.audioPlot.updateBuffer(buffer[0], withBufferSize: bufferSize)
+    }
   }
 }
